ee246f82cf9596bbf228893158e8aec07db0de00
Lucas-Qian6 <yq2432@columbia.edu>
2025-12-10 09:49:19 -0500

    configure env

--- hpml_testing/benchmark_ring_vs_regular.sh
+++ hpml_testing/benchmark_ring_vs_regular.sh
@@ -12,7 +12,7 @@ set -e
 #"/datasets/ai/llama3/hub/models--meta-llama--Llama-3.2-1B/snapshots/4e20de362430cd3b72f300e6b0f18e50e7166e08"
 #"/datasets/ai/llama3/hub/models--meta-llama--Llama-3.1-8B/snapshots/d04e592bb4f6aa9cfee91e2e20afa771667e1d4b"
 #"/datasets/ai/llama3/hub/models--meta-llama--Llama-3.1-8B/snapshots/d04e592bb4f6aa9cfee91e2e20afa771667e1d4b"
-MODEL_PATH="/workspace/llama-hf"
+MODEL_PATH="/workspace/llama-hf/original"
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 RESULTS_DIR="benchmark_results"
 NUM_GPUS=2
